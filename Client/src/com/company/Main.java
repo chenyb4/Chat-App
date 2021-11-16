9da35e821a9a9fe63437781eab2aea9f0b014c61
Lukman Al-Busaidi <luqman-22@hotmail.com>
2021-11-16 13:12:27 +0100

    Client and server class is added

--- Client/src/com/company/Main.java
+++ Client/src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Server server = new Server();
+        try {
+            server.start(1337);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() throws IOException {
+        Client client = new Client();
+        client.startConnection("127.0.0.1", 1337);
+        String response = client.sendMessage("hello server");
+        assertEquals("hello client", response);
     }
 }
