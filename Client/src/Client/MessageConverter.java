bbbe0d3d54d3a44c07629bb167d07c053397d184
Lukman Al-Busaidi <luqman-22@hotmail.com>
2021-12-22 11:46:25 +0100

    Added ER13 error message. Signed-off-by: Lukman Al-Busaidi <luqman-22@hotmail.com>

--- Client/src/Client/MessageConverter.java
+++ Client/src/Client/MessageConverter.java
@@ -14,7 +14,7 @@ public class MessageConverter {
         }
 
         // if it is not the case of message not sent, then we care be sure that
-        //we can seperate the message by space and analyze the parts
+        //we can separate the message by space and analyze the parts
         String[] lineParts=m.split(" ");
 
         //the name is being converted in a format that it lets the users know whether the sender of
@@ -64,38 +64,30 @@ public class MessageConverter {
                 return nameWithStar+" left group "+groupName+".";
             }
 
-
-
-
-            case "ER01"->{return "This user name is already used. Please choose a different username!";}
-            case "ER02"->{return "The username you enter has an invalid format. Only characters, numbers and underscores are allowed!";}
-            case "ER03"->{return "Please log in first!";}
-            case "ER04"->{return "The user does not exist.";}
-            case "ER05"->{return "This group name is already in use.";}
-            case "ER06"->{return "The group name you entered has an invalid format. Your group name should not contain space.";}
-            case "ER07"->{return "The group you wanted to join does not exist.";}
-            case "ER08"->{return "You are currently not a member of this group. Please join the group first.";}
-            case "ER09"->{return "You are already in this group.";}
-            case "ER10"->{return "The password has an invalid format(the password should be between 6 - 20 characters)";}
-            case "ER11"->{return "You are already authenticated.";}
-            case "ER12"->{return "You cannot send an empty message.";}
-            case "DSCN"->{return "You have disconnected from the server due to inactivity.";}
-            case "MSG"->{return "You entered an invalid message.";}
-            case "ER00"->{ return "There is no such command. Please enter '?' to see the manual.";}
+            case "ER01" -> {return "This user name is already used. Please choose a different username!";}
+            case "ER02" -> {return "The username you enter has an invalid format. Only characters, numbers and underscores are allowed!";}
+            case "ER03" -> {return "Please log in first!";}
+            case "ER04" -> {return "The user does not exist.";}
+            case "ER05" -> {return "This group name is already in use.";}
+            case "ER06" -> {return "The group name you entered has an invalid format. Your group name should not contain space.";}
+            case "ER07" -> {return "The group you wanted to join does not exist.";}
+            case "ER08" -> {return "You are currently not a member of this group. Please join the group first.";}
+            case "ER09" -> {return "You are already in this group.";}
+            case "ER10" -> {return "The password has an invalid format(the password should be between 6 - 20 characters)";}
+            case "ER11" -> {return "You are already authenticated.";}
+            case "ER12" -> {return "You cannot send an empty message.";}
+            case "DSCN" -> {return "You have disconnected from the server due to inactivity.";}
+            case "MSG" -> {return "You entered an invalid message.";}
+            case "ER00" -> { return "There is no such command. Please enter '?' to see the manual.";}
             default -> {}
         }
-
         if (m.contains("INFO")){
             return "Welcome to the chat server!";
         }
-
         if(m==null){
             return "(message not sent)";
         }
-
         return "";
-
-
     }
 
 
